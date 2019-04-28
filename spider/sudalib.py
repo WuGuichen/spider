2de94756715bfc806debc55a0b0a824fc167802d
WuGuichen <577024128@qq.com>
2019-04-29 02:31:17 +0800

    简易查询

--- spider/sudalib.py
+++ spider/sudalib.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 base_url = 'http://opac.suda.edu.cn/m/weixin/wsearch.action'
 question = input("请输入要查找的问题的关键字：")
+page_num = int(input("输入查找页数："))
 paylord = {'q': question, 't': 'any'}
 headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.\
@@ -30,13 +31,23 @@ def get_text(result):
         i = i.string
         n = n.string
         final_text[i] = n
+    count =  1
+    print("=======================")
     for k,v in final_text.items():
-        print(k,v)
+        print(count,end='>  ')
+        count += 1
+        print('《{0}》'.format(k),end='  ------  ')
+        print(v)
+    print("========下一页=========")
 
 
 if __name__ == '__main__':
+    for num in range(1,page_num+1):
+        paylord['page'] = num
+        result = url_open(base_url,headers)
+        result = result.text
+        get_text(result)
 
-    result = url_open(base_url,headers)
-    result = result.text
-    get_text(result)
+
+
 
